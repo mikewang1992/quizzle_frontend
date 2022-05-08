20adb077009351ce7147289d13a8a95aee08096b
DESKTOP-MQHH5LU\Mike Wang <wsp7190580@gmail.com>
2022-05-08 14:47:24 +1000

    jquery fix + api adjust

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 $( document ).ready(function() {
     console.log( "jQuery ready!" );
-
+    // $.ajax({url: "http://localhost:8081/", success: function(result){
+    //   console.log(result);
+    // }});
     // Make a request for a user with a given ID
-    axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    // axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    //   .then(function (response) {
+    //     console.log('axios ready, dummy api respose')
+    //     // handle success
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    //   })
+    // func/populate
+      axios.get('http://localhost:8081/quiz/6275de35a3c5289e6d24656d')
       .then(function (response) {
-        console.log('axios ready, dummy api respose')
         // handle success
         console.log(response);
       })
@@ -12,7 +24,4 @@ $( document ).ready(function() {
         // handle error
         console.log(error);
       })
-      .then(function () {
-        // always executed
-      });
 });

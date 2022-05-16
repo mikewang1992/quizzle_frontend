a4c01f371cf32fb78742285a415ca02c9745fb54
DESKTOP-MQHH5LU\Mike Wang <wsp7190580@gmail.com>
2022-05-16 20:32:22 +1000

    contact page, hide sign up

--- js/contact.js
+++ js/contact.js
@@ -1,27 +1,7 @@
 $( document ).ready(function() {
     console.log( "jQuery ready!" );
-    // $.ajax({url: "http://localhost:8081/", success: function(result){
-    //   console.log(result);
-    // }});
-    // Make a request for a user with a given ID
-    // axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    //   .then(function (response) {
-    //     console.log('axios ready, dummy api respose')
-    //     // handle success
-    //     console.log(response);
-    //   })
-    //   .catch(function (error) {
-    //     // handle error
-    //     console.log(error);
-    //   })
-    // func/populate
-      axios.get('http://localhost:8081/quiz/6275de35a3c5289e6d24656d')
-      .then(function (response) {
-        // handle success
-        console.log(response);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
+    $('.contact_submit').click(function() {
+      $('.contact_form').hide()
+      $('.getbackmesg').show()
+    })
 });
